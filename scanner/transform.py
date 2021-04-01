4c3d88301434daf9ef15c87ba43458336822258f
Veershah26 <shah.veer18@gmail.com>
2021-04-01 15:40:53 +0530

    Added Perspective Transform

--- scanner/transform.py
+++ scanner/transform.py
@@ -34,7 +34,7 @@ def four_point_transform(image, pts):
         [maxHeight - 1, maxHeight - 1],
         [0, maxHeight -1]], dtype = "float32")
 
-    #M = cv2.getPerspectiveTransform(rect, dst)
+    M = cv2.getPerspectiveTransform(rect, dst)
     #warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
     #return warped
