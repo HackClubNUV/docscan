7148bb5576f8920dd91df2d38e5ccc9e33b9fa45
Veershah26 <shah.veer18@gmail.com>
2021-04-01 15:38:43 +0530

    Added gross distance function

--- scanner/transform.py
+++ scanner/transform.py
@@ -27,3 +27,14 @@ def four_point_transform(image, pts):
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB)) 
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxHeight - 1, maxHeight - 1],
+        [0, maxHeight -1]], dtype = "float32")
+
+    #M = cv2.getPerspectiveTransform(rect, dst)
+    #warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    #return warped
